d849a6e6e2c88c99f7dd7ff612f9994ef1854a70
MinhHuong <minh_huong_148@yahoo.com>
2013-10-22 20:13:35 +0700

    Modification 22/10

--- CasseBrique/casse_brique_ensemble.cpp
+++ CasseBrique/casse_brique_ensemble.cpp
@@ -50,7 +50,7 @@ void mouvement_balle (Sprite & s);
 
 int main()
 {     
-                                          /* DECLARER LES VARIABLES */
+  /* DECLARER LES VARIABLES */
   
   Sprite balle;
   init_element (balle, 17, 8, BALLE_DX, BALLE_DY);
@@ -66,59 +66,60 @@ int main()
   for (int m = 0 ; m < LIG_BRI ; m++)
     for (int n = 0 ; n < C_BRI ; n++)
       { 
-	brique[m][n].a = m + 2; 
-	for (int e = 0 ; e < LAR_BRI ; e++)
-	  { brique[m][n].presente = true; 
-	    brique[m][n].b[e] = 8*n + 2 + e; }// le coefficient (8) decide la distance entre les colonnes de briques  
+        brique[m][n].a = m + 2; 
+        for (int e = 0 ; e < LAR_BRI ; e++)
+          { brique[m][n].presente = true; 
+            brique[m][n].b[e] = 8*n + 2 + e; }// le coefficient (8) decide la distance entre les colonnes de briques  
       }      
   // la premiere brique sera affichee aux coordonnees (2;2)
 
 
+  int test = 0 ; // 1 variable supplementaire pour terminer le jeu quand toutes les briques disparaissent 
 
 /**************************************************************************************************************/
 
-                                   /* CONTROLER LE GALLET AVEC LE CLAVIER */
+  /* CONTROLER LE GALLET AVEC LE CLAVIER */
 
   for (int t = 0 ; ; t++)
     { system ("stty raw");   
       usleep (30 * 1000);
-	  
+          
       if (touche_appuyee()) 
-	{ char c = getchar();
-	  system("stty cooked");
-	  
-	  for (int k = 0 ; k < LAR_GAL ; k++)
-	    { switch (c)
-		{ case 'f' : 
-		    gallet[k].y -= 2;
-		    break;
- 		  case 'g' :
-		    gallet[k].y += 2; 
-		    break;
-		  case 13 :
-		    system ("stty cooked");
-		    exit(0); }
-	    }
-	}	    
+        { char c = getchar();
+          system("stty cooked");
+          
+          for (int k = 0 ; k < LAR_GAL ; k++)
+            { switch (c)
+                { case 'f' : 
+                    gallet[k].y -= 2;
+                    break;
+		case 'g' :
+		  gallet[k].y += 2; 
+		  break;
+		case 13 :
+		  system ("stty cooked");
+		  exit(0); }
+            }
+        }            
     
       system ("stty cooked");
 
 /**************************************************************************************************************/
 
-                               /* LIMITER L'ESPACE DE DEPLACEMENT DU GALLET */
+      /* LIMITER L'ESPACE DE DEPLACEMENT DU GALLET */
 
       for (int k = 0 ; k < LAR_GAL ; k++) 
-	if (gallet[LAR_GAL - 1].y > COLONNE - 1)
-	  gallet[k].y -= 2;
+        if (gallet[LAR_GAL - 1].y > COLONNE - 1)
+          gallet[k].y -= 2;
     
 
       for (int l = LAR_GAL - 1 ; l >= 0 ; l--)      
-	if (gallet[0].y < 1)
-	  gallet[l].y += 2;
+        if (gallet[0].y < 1)
+          gallet[l].y += 2;
 
 /***************************************************************************************************************/
 
-                          /* AFFICHER LES ELEMENTS : CADRE, BALLE, GALLET, BRIQUES */
+      /* AFFICHER LES ELEMENTS : CADRE, BALLE, GALLET, BRIQUES */
 
       cadre();
 
@@ -127,64 +128,55 @@ int main()
 
 
       for (int k = 0 ; k < LAR_GAL ; k++)
-	ecran[(int) gallet[k].x][(int) gallet[k].y] = '=';
+        ecran[(int) gallet[k].x][(int) gallet[k].y] = '=';
 
 
       for (int m = LIG_BRI ; m >= 0 ; m--)  // detecter si la balle touche les briques
-	for (int n = 0 ; n < C_BRI ; n++)
-	  for (int e = 0 ; e < LAR_BRI ; e++)
-	    if (   (int) balle.x + 1 == brique[m][n].a  
-		   && (int) balle.y == brique[m][n].b[e] 
-		   && brique[m][n].presente == true) 
+        for (int n = 0 ; n < C_BRI ; n++)
+          for (int e = 0 ; e < LAR_BRI ; e++)
+            if (   (int) balle.x + 1 == brique[m][n].a  
+                   && (int) balle.y == brique[m][n].b[e] 
+                   && brique[m][n].presente == true) 
 
-	      { balle.dx = BALLE_DX;        // la balle rebondit
-		brique[m][n].presente = false; } 
-	
+              { balle.dx = BALLE_DX;        // la balle rebondit
+                brique[m][n].presente = false; 
+		test += 1; } // quand la balle touche 1 brique, le programme effectue test += 1; 
+        
       // balle_brique (brique[LIG_BRI][C_BRI], balle); 
       // Nous avons essaye a utiliser cette fonction pour rendre les codes plus clairs, mais elle ne marche pas :(
 
 
       for (int m = 0 ; m < LIG_BRI ; m++)
-	for (int n = 0 ; n < C_BRI ; n++)
-	  for (int e = 0 ; e < LAR_BRI ; e++)
-	    if (brique[m][n].presente)  
-	      ecran[brique[m][n].a][brique[m][n].b[e]] = 'H'; 
+        for (int n = 0 ; n < C_BRI ; n++)
+          for (int e = 0 ; e < LAR_BRI ; e++)
+            if (brique[m][n].presente)  
+              ecran[brique[m][n].a][brique[m][n].b[e]] = 'H'; 
       // afficher seulement les briques que la balle ne touchent pas encore
-	
-	
+        
+        
       affichage_ecran(); 
       
 /****************************************************************************************************************/
 
-                                       /* LE MOUVEMENT DE LA BALLE */
+      /* LE MOUVEMENT DE LA BALLE */
 
 
       mouvement_balle(balle);
 
 
       if ((int) balle.x == 19)
-	for (int k = 0 ; k < LAR_GAL ; k++)
-	  if ((int)balle.y == (int)gallet[k].y)
-	    balle.dx = - BALLE_DX; 
+        for (int k = 0 ; k < LAR_GAL ; k++)
+          if ((int)balle.y == (int)gallet[k].y)
+            balle.dx = - BALLE_DX; 
 
 
-      if (      brique[0][0].presente == false 
-	     && brique[0][1].presente == false 
-	     && brique[0][2].presente == false 
-	     && brique[0][3].presente == false 
-	     && brique[0][4].presente == false 
-	     && brique[0][5].presente == false 
-	     && brique[0][6].presente == false 
-	     && brique[0][7].presente == false 
-	     && brique[0][8].presente == false )
-	{ cout << endl << "----- VOUS AVEZ GAGNE -----" << endl << endl;
+      if (test >= 54) //total : 54 briques, quand test atteint ce nombre, toutes les briques disparaissent -> gagner
+	{ cout << endl << "VOUS AVEZ GAGNE" << endl << endl; 
 	  exit(0); }
-      // si toutes les briques disparaissent, le joueur va gagner
-      // nous avons aussi essaye a utiliser une boucle "for" pour brique[0][n].presente, mais ell ne marche pas =(
-
+    
 
       if (balle.x >= LIGNE - 2)
-	{ cout << endl << "----- GAME OVER -----" << endl << endl;
+        { cout << endl << "----- GAME OVER -----" << endl << endl;
 	  exit(0); }  
       // si la balle tombe du gallet, le joueur va perdre
           
@@ -204,10 +196,10 @@ void cadre ()
 {
   for (int i = 0 ; i < LIGNE ; i++)
     { for (int j = 0 ; j < COLONNE ; j++)
-	if (i == 0 || i == LIGNE - 1 || j == 0 || j == COLONNE - 1)
-	  ecran[i][j] = '#';
-	else
-	  ecran[i][j] = ' '; }
+        if (i == 0 || i == LIGNE - 1 || j == 0 || j == COLONNE - 1)
+          ecran[i][j] = '#';
+        else
+          ecran[i][j] = ' '; }
 }
 
 
@@ -216,7 +208,7 @@ void affichage_ecran()
   system("clear");
   for (int i = 0 ; i < LIGNE ; i++)
     { for (int j = 0 ; j < COLONNE ; j++)
-	cout << ecran[i][j];
+        cout << ecran[i][j];
       cout << endl; }
 }
 
@@ -226,12 +218,12 @@ void balle_brique(Block & p, Sprite & s)
   for (int m = LIG_BRI ; m >= 0 ; m--)
     for (int n = 0 ; n < C_BRI ; n++)
       for (int e = 0 ; e < LAR_BRI ; e++)
-	if (    (int)s.x + 1 == p.a 
-		&& (int)s.y == p.b[e] 
-		&& p.presente == true)
-	  
-	  { s.dx = BALLE_DX; 
-	    p.presente = false; }
+        if (    (int)s.x + 1 == p.a 
+                && (int)s.y == p.b[e] 
+                && p.presente == true)
+          
+          { s.dx = BALLE_DX; 
+            p.presente = false; }
 }
 
 
--- CasseBrique/readme.txt
+++ CasseBrique/readme.txt
@@ -1,12 +1,12 @@
-**************************************************************************************************************
-**************************************************************************************************************
-**************************************************************************************************************
+****************************************************************************************************
+****************************************************************************************************
+****************************************************************************************************
 
-                                    --- LE PROJET DU JEU "CASSE BRIQUE" ---
+                             --- LE PROJET DU JEU "CASSE BRIQUE" ---
 
-**************************************************************************************************************
-**************************************************************************************************************
-**************************************************************************************************************
+****************************************************************************************************
+****************************************************************************************************
+****************************************************************************************************
 
 REPONSE A QUESTION 1 : le fichier "casse_brique_balle.cpp"
 
@@ -16,7 +16,7 @@ Dans ce fichier nous avons mis les codes dont les fonctions :
 
 - la balle qui va rebondir chaque fois qu'elle est en contact avec les murs "O"
 
-**************************************************************************************************************
+****************************************************************************************************
 
 REPONSE A QUESTION 2 : le fichier "casse_brique_gallet.cpp"
 
@@ -26,7 +26,7 @@ Dans ce fichier nous avons ajoute les codes dont les fonctions :
 
 - la balle qui va rebondir chaque fois qu'elle est en contact avec la gallet
  
-**************************************************************************************************************
+****************************************************************************************************
 
 REPONSE A QUESTION 3 : le fichier "casse_brique_ensemble.cpp"
 
@@ -40,7 +40,7 @@ Dans ce fichier nous avons mis les codes pour l'ensemble du programme :
 
   si la balle fait disparaitre toutes les briques : le joueur gagne.
 
-**************************************************************************************************************
+*****************************************************************************************************
 
 LES ERREURS DANS NOTRE PROGRAMME :
 
@@ -52,28 +52,6 @@ Bien que notre jeu marche (au moins il n'y a pas de fautes graves), nous avons e
 
 2) La partie ou on detecte si la balle touche les briques, les fait disparaitre et rebondit : nous ecrivons une fonction (void balle_brique (...), nous la mettons dans les commentaire //) afin de rendre le "main" plus propre. Quand on compile le fichier, le Terminal n'avertit aucune faute, mais la balle ne rebondit plus quand elle touche les briques. C'est pourquoi, nous mettons tous les codes necessaires dans le "main".
 
-
-3) La partie ou le programme decide si le joueur gagne :
-
-Les codes ci-dessous :
-
-if (    brique[0][0].presente == false 
-     && brique[0][1].presente == false 
-     && brique[0][2].presente == false
-     && brique[0][3].presente == false 
-     && brique[0][4].presente == false 
-     && brique[0][5].presente == false 
-     && brique[0][6].presente == false 
-     && brique[0][7].presente == false 
-     && brique[0][8].presente == false   )
-   { cout << endl << "----- VOUS AVEZ GAGNE -----" << endl << endl;
-     exit(0); }
-
-Ils sont terribles...
-
-Notre idee : le joueur va gagner si la balle detruit toutes les briques de la derniere ligne, c'est-a-dire toutes les briques a la derniere ligne sont attribuees de bool FALSE. 
-
-Seulement la derniere ligne est necessaire, donc on prend "brique[0][n].presente", puis nous voulons utiliser la boucle for pour n, mais finalement nous ne savons pas comment faire ca sans afficher plusieurs fois la ligne "VOUS AVEZ GAGNE".
 
 
 
